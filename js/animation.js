8fc58d840e773c53dcef35b3391b61b65a407f7e
RItesh <shuklaritesh5678@gmail.com>
2023-09-19 20:42:45 +0530

    added scrollBar Animation to the nav links and added my original profile pic

---
+++ js/animation.js
@@ -0,0 +1,20 @@
+// // Add smooth scrolling to all links with hashes
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+ 
+
+
