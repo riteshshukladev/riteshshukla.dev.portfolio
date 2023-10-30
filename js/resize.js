6d0a10749ecfd9177220d3a213ec841ca0da6421
RItesh <shuklaritesh5678@gmail.com>
2023-10-30 11:25:50 +0530

    Some Minor Modification

--- js/resize.js
+++ js/resize.js
@@ -1,20 +1,61 @@
+// const pr1 = [
+//     "The Weather App provides real-time global weather data, offering easy and speedy access to weather information for any city. Use a user-friendly interface to select predefined cities for quick updates or enter your desired location. Dynamic backgrounds and icons reflect current weather conditions, creating a visual connection to the elements.",
+//     "The Weather App simplifies accessing real-time global weather. Quickly find weather data for any city with its user-friendly interface. It offers predefined city options for swift updates or allows manual entry.Enjoy dynamic backgrounds and icons reflecting current conditions, all powered by the reliable OpenWeather API.",
+//     "Experience instant access to real-time global weather with the Weather App. It offers quick access to predefined cities, dynamic visuals, and precise data powered by OpenWeather API. Stay informed and travel confidently with ease.User-friendly error handling guides you in case of input errors or API issues. "
+// ]
+
+// function updateTextBasedOnScreenSize() {
+//     const textContainer = document.querySelector(".pr-details");
+//     const screenWidth = window.innerWidth;
+
+//     if (screenWidth <= 524) {
+//         textContainer.textContent = pr1[2];
+//     } else if (screenWidth <= 1024) { 
+//         textContainer.textContent = pr1[1];
+//     } else {
+//         textContainer.textContent = pr1[0];
+//     }
+// }
+
+// // Initial call to set the text based on the current screen size
+// updateTextBasedOnScreenSize();
+
+// // Add an event listener to update the text when the screen size changes
+// window.addEventListener("resize", updateTextBasedOnScreenSize);
+
+
 const pr1 = [
-    "The Weather App provides real-time global weather data, offering easy and speedy access to weather information for any city. Use a user-friendly interface to select predefined cities for quick updates or enter your desired location. Dynamic backgrounds and icons reflect current weather conditions, creating a visual connection to the elements.",
-    "The Weather App simplifies accessing real-time global weather. Quickly find weather data for any city with its user-friendly interface. It offers predefined city options for swift updates or allows manual entry.Enjoy dynamic backgrounds and icons reflecting current conditions, all powered by the reliable OpenWeather API.",
-    "Experience instant access to real-time global weather with the Weather App. It offers quick access to predefined cities, dynamic visuals, and precise data powered by OpenWeather API. Stay informed and travel confidently with ease.User-friendly error handling guides you in case of input errors or API issues. "
-]
+    {
+        obj1: {
+            text1: "The Weather App provides real-time global weather data, offering easy and speedy access to weather information for any city. Use a user-friendly interface to select predefined cities for quick updates or enter your desired location. Dynamic backgrounds and icons reflect current weather conditions, creating a visual connection to the elements.",
+            text2: "The Weather App simplifies accessing real-time global weather. Quickly find weather data for any city with its user-friendly interface. It offers predefined city options for swift updates or allows manual entry. Enjoy dynamic backgrounds and icons reflecting current conditions, all powered by the reliable OpenWeather API.",
+            text3: "Experience instant access to real-time global weather with the Weather App. It offers quick access to predefined cities, dynamic visuals, and precise data powered by OpenWeather API. Stay informed and travel confidently with ease. User-friendly error handling guides you in case of input errors or API issues."
+        }
+    },
+    {
+        obj2 : {
+            text: "Experience the future of online shopping with our HTML, CSS, and JS-powered E-commerce clone Named Cara. Seamlessly browse a diverse catalog, enjoy an intuitive user interface, and revel in a visually stunning frontend design."
+        }
+    }
+];
+
 
 function updateTextBasedOnScreenSize() {
-    const textContainer = document.querySelector(".pr-details");
+    const textContainer1 = document.querySelector(".pr-details__first");
+    const textContainer2 = document.querySelector(".pr-details__second");
     const screenWidth = window.innerWidth;
 
-    if (screenWidth <= 524) {
-        textContainer.textContent = pr1[2];
-    } else if (screenWidth <= 1024) { 
-        textContainer.textContent = pr1[1];
-    } else {
-        textContainer.textContent = pr1[0];
-    }
+        if (screenWidth <= 524) {
+            textContainer1.textContent = pr1[0].obj1.text3;
+            // textContainer2.textContent = pr1[1].obj2.text;
+        } else if (screenWidth <= 1024) {
+            textContainer1.textContent = pr1[0].obj1.text2;
+            // textContainer2.textContent = pr1[1].obj2.text;
+        } else {
+            textContainer1.textContent = pr1[0].obj1.text1;
+            // textContainer2.textContent = pr1[1].obj2.text;
+        }
+        textContainer2.textContent = pr1[1].obj2.text;
 }
 
 // Initial call to set the text based on the current screen size
